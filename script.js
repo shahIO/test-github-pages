1f7d7d7aafdcd9d9203255bc6ef2050fbadd525b
shah <shah.sync@outlook.com>
2024-12-20 15:16:16 +0530

    contact me

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', () => {
     document.body.classList.add('loaded');
 });
+
+function openGmail() {
+    const email = "shah.sync@outlook.com";
+    const subject = encodeURIComponent("");
+    const body = encodeURIComponent("");
+
+    const mailtoUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`;
+    const a = document.createElement('a');
+    a.href = mailtoUrl;
+    a.target = "_blank";
+    a.click();
+}
